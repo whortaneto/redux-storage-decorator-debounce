2cf5b53049c855008c6d9f17ff836ac6e1aa52be
whortaneto <wneto@wagnerhorta.com>
2017-09-06 11:20:29 -0300

    fixing maxWait

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import debounce from 'lodash.debounce';
+
 export default (engine, ms, maxWait = null, eventsToPersistOn = ['beforeunload']) => {
     if (maxWait !== null && ms > maxWait) {
         throw new Error('maxWait must be > ms');
     }
 
-    let lastTimeout;
-    let maxTimeout;
+    let debounced = false;
     let lastReject;
     let lastState;
 
@@ -16,24 +17,26 @@ export default (engine, ms, maxWait = null, eventsToPersistOn = ['beforeunload']
     }
     if (hasWindow && window.addEventListener) {
         const saveUponEvent = () => {
-            if (!lastTimeout) {
+            if (!debounced) {
                 return;
             }
-
-            lastTimeout = clearTimeout(lastTimeout);
-            maxTimeout = clearTimeout(maxTimeout);
             lastReject = null;
             engine.save(lastState);
         };
         eventsToPersistOn.forEach(eventName => window.addEventListener(eventName, saveUponEvent));
     }
 
+
+    const debouncedSave = debounce((stateToSave, resolve, reject) => {
+        debounced = false;
+        engine.save(stateToSave).then(resolve).catch(reject);
+    }, ms, { maxWait });
+
     return {
         ...engine,
 
         save(state) {
             lastState = state;
-            lastTimeout = clearTimeout(lastTimeout);
 
             if (lastReject) {
                 lastReject(Error('Debounced, newer action pending'));
@@ -41,20 +44,9 @@ export default (engine, ms, maxWait = null, eventsToPersistOn = ['beforeunload']
             }
 
             return new Promise((resolve, reject) => {
-                const doSave = () => {
-                    lastTimeout = clearTimeout(lastTimeout);
-                    maxTimeout = clearTimeout(maxTimeout);
-                    lastReject = null;
-                    lastState = null;
-                    engine.save(state).then(resolve).catch(reject);
-                };
-
                 lastReject = reject;
-                lastTimeout = setTimeout(doSave, ms);
-
-                if (maxWait !== null && !maxTimeout) {
-                    maxTimeout = setTimeout(doSave, maxWait);
-                }
+                debounced = true;
+                debouncedSave(state, resolve, reject);
             });
         }
     };
